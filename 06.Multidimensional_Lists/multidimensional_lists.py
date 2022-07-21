bbf58aea6aa59d5d1c98ecdc9948d3323cdcbc10
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-21 19:57:53 +0300

    Add files via upload

---
+++ 06.Multidimensional_Lists/multidimensional_lists.py
@@ -0,0 +1,42 @@
+"""
+1. Definition and usage - Lists within lists
+- Grid
+- Cube
+
+- Used in graphics
+- Tabular data
+- Game development
+- Machine learning and AI
+
+2. Creating MD list with zeros using loops
+matrix = []
+for i in range(3)
+    matrix.append([])
+
+
+
+"""
+# Single dimension
+list_one = [1, 2, 3, 4, 5, 6, 7]
+
+# Multi dimension list - Two dimensions - List of lists
+multi_list = [
+    [1, 2, 3],
+    [3, 4, 5],
+    [5, 6, 7],
+    [7, 8, 9]
+]
+# Three dimension list
+cc = [
+    [
+        [1, 2, 3],
+        [2, 3, 4],
+        [3, 4, 5],
+        [4, 5, 6]
+    ],
+    [
+        [5, 6, 7],
+        [6, 7, 8],
+        [7, 8, 9],
+    ]
+]
