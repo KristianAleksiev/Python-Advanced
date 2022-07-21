0ada3751d05e3955e74a83762eef2125b7e9db01
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-21 19:58:06 +0300

    Add files via upload

---
+++ 06.Multidimensional_Lists/primary_diagonal.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+matrix = []
+
+for _ in range(n):
+    matrix.append([int(x) for x in input().split()])
+
+
+def get_primary_diagonal_sum(matrix):
+    the_sum = 0
+    n = len(matrix)
+    # for r in range(n):
+    #     for c in range(n):
+    #         if r == c:
+    #             the_sum += matrix[r][c]
+    for i in range(n):
+        the_sum += matrix[i][i]
+    return the_sum
+
+
+print(get_primary_diagonal_sum(matrix))
