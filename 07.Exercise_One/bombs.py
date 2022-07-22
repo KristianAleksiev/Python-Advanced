69177860049af5ac4795fd4e54aee984ca3fad27
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-22 22:00:25 +0300

    Add files via upload

---
+++ 07.Exercise_One/bombs.py
@@ -0,0 +1,56 @@
+def get_neighbours(matrix, row, col):
+    possible_children_coordinates = [
+        [row - 1, col - 1],
+        [row - 1, col],
+        [row - 1, col + 1],
+        [row, col - 1],
+        [row, col + 1],
+        [row + 1, col - 1],
+        [row + 1, col],
+        [row + 1, col + 1]
+    ]
+
+    result = []
+
+    for child_row, child_col in possible_children_coordinates:
+        if 0 <= child_row < len(matrix) and 0 <= child_col < len(matrix) and matrix[child_row][child_col] > 0:
+            result.append([child_row, child_col])
+    return result
+
+
+size = int(input())
+matrix = []
+
+for _ in range(size):
+    matrix.append([int(x) for x in input().split()])
+
+bombs = input().split()
+
+for bomb in bombs:
+    row, col = [int(x) for x in bomb.split(",")]
+    power = matrix[row][col]
+
+    if power <= 0:
+        continue
+
+    matrix[row][col] = 0
+
+    children = get_neighbours(matrix, row, col)
+
+    for child_row, child_col in children:
+        matrix[child_row][child_col] -= power
+
+alive_cells = 0
+alive_cells_sum = 0
+
+for row in matrix:
+    for element in row:
+        if element > 0:
+            alive_cells += 1
+            alive_cells_sum += element
+
+print(f"Alive cells: {alive_cells}")
+print(f"Sum: {alive_cells_sum}")
+
+for row in matrix:
+    print(*row, sep=" ")
