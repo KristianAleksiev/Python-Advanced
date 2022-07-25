7b4b51ad9cc60593e41ba424ef1fc1a806c7826d
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-25 18:57:41 +0300

    Add files via upload

---
+++ 11.Error_Handling/error_handling.py
@@ -0,0 +1,8 @@
+"""
+Two types of distinguishable errors - SyntaxError, Exception
+
+Exceptions - Valid python code (syntax), which doesn't work properly
+Exceptions gives us information about the problems in the execution of the code
+Exceptions need to be managed
+
+"""
