862cab3ba39b546bfc5a57a9f00046f52c739d34
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-26 21:11:57 +0300

    Add files via upload

---
+++ 13.Modules/modules.py
@@ -0,0 +1,23 @@
+"""
+1. Definition -
+Code with functionality, written and stored in packages which is reusable
+Each module has a specific purpose of usage
+
+2. Built-in modules
+Pre-installed in python Runtime environment (cpython, PyPy)
+Both cpython and PyPy use python language, but it behaves differently in each
+
+
+
+3. External modules
+Package manager - pip install, pip uninstall, pip freeze - all packages installed
+pip freeze > requirements.txt
+pip install -r requirements.txt
+
+4. Custom modules
+Any .py file can be imported and used as a module
+
+Creating custom package - + __init__
+from triangle import print_triangle - Function from folder (__init__.py)
+from triangle.line import print_line - Function from folder (line.py)
+"""
